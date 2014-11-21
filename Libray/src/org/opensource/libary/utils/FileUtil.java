4f60863431f61eb4d7b65d9fbcabfb3e1c7890e9
fuqiang <Administrator@WIN-IQKKDU26TFE>
2014-11-21 14:38:17 +0800

    添加读取文件大小，删除文件

--- Libray/src/org/opensource/libary/utils/FileUtil.java
+++ Libray/src/org/opensource/libary/utils/FileUtil.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
+import java.text.DecimalFormat;
 
 import org.opensource.libary.AppStart;
 
@@ -432,7 +433,12 @@ public final class FileUtil
 		return buffer.toString();
 	}
 	
-	//批量更改文件后缀
+	/**
+	 * 批量更改文件后缀
+	 * @param dir
+	 * @param oldSuffix
+	 * @param newSuffix
+	 */
 	public static void reNameSuffix(File dir,String oldSuffix,String newSuffix)
 	{
 		if (dir.isDirectory())  
@@ -466,5 +472,73 @@ public final class FileUtil
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	/**
+	 * 换算文件大小
+	 * 
+	 * @param size
+	 * @return
+	 */
+	public static String formatFileSize(long size) {
+		DecimalFormat df = new DecimalFormat("#.00");
+		String fileSizeString = "未知大小";
+		if (size < 1024) {
+			fileSizeString = df.format((double) size) + "B";
+		} else if (size < 1048576) {
+			fileSizeString = df.format((double) size / 1024) + "K";
+		} else if (size < 1073741824) {
+			fileSizeString = df.format((double) size / 1048576) + "M";
+		} else {
+			fileSizeString = df.format((double) size / 1073741824) + "G";
+		}
+		return fileSizeString;
+	}
+	
+	/**
+	 * 删除文件
+	 * 
+	 * @param path
+	 *            文件的路径
+	 */
+	public static void delAllFile(String path) {
+		File file = new File(path);
+		if (!file.exists()) {
+			return;
+		}
+		if (!file.isDirectory()) {
+			return;
+		}
+		String[] tempList = file.list();
+		File temp = null;
+		for (int i = 0; i < tempList.length; i++) {
+			if (path.endsWith(File.separator)) {
+				temp = new File(path + tempList[i]);
+			} else {
+				temp = new File(path + File.separator + tempList[i]);
+			}
+			if (temp.isFile()) {
+				temp.delete();
+			}
+			if (temp.isDirectory()) {
+				delAllFile(path + "/" + tempList[i]);
+				delFolder(path + "/" + tempList[i]);
+			}
+		}
+	}
+	
+	/**
+	 * 删除文件夹
+	 * 
+	 * @param folderPath
+	 *            文件夹的路径
+	 */
+	public static void delFolder(String folderPath) {
+		delAllFile(folderPath);
+		String filePath = folderPath;
+		filePath = filePath.toString();
+		java.io.File myFilePath = new java.io.File(filePath);
+		myFilePath.delete();
 	}
 }
